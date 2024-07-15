de5054b52c483fecd7380ae41b0d1924246b7a97
tsuzuki-takaaki <echo.tsuzuki.takaaki@gmail.com>
2024-07-15 18:36:27 +0900

    Add remigrate example

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,10 @@
 # https://github.com/tsuzuki-takaaki/isucon/blob/main/setup/restart.sh
-# TODO: migrationの追加
+# TODO: migrationの追加 like this ↓↓↓↓
+
+# Recreate database
+# sudo mysql -e "DROP DATABASE isupipe"
+# sudo mysql -e "CREATE DATABASE isupipe"
+# Migrate with new schema
+# sudo mysql -D isupipe < /home/isucon/webapp/sql/initdb.d/10_schema.sql
+# Seed
+# ~/webapp/sql/init.sh
